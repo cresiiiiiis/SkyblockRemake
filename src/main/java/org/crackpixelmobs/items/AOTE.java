04f6af3c5cd582c06a17fe8faa6524a006373774
ProxyD_ <87957190+Dctrrr@users.noreply.github.com>
2023-08-27 16:27:36 +0530

    Update AOTE.java

--- src/main/java/org/crackpixelmobs/items/AOTE.java
+++ src/main/java/org/crackpixelmobs/items/AOTE.java
@@ -36,15 +36,15 @@ public class AOTE implements Listener {
         aoteMeta.setDisplayName(ChatColor.BLUE + "Aspect of The End");
 
         List<String> lore = new ArrayList<>();
-        lore.add(ChatColor.GRAY + "Damage: " + ChatColor.RED + "+"); // Closing the add call
-        lore.add(ChatColor.GRAY + "Strength: " + ChatColor.RED + "+"); // Closing the add call
+        lore.add(ChatColor.GRAY + "Damage: " + ChatColor.RED + "+100"); // Closing the add call
+        lore.add(ChatColor.GRAY + "Strength: " + ChatColor.RED + "+100"); // Closing the add call
         lore.add(ChatColor.GOLD + "Ability: Instant Transmission" + ChatColor.YELLOW + ChatColor.BOLD + " RIGHT CLICK"); // Closing the add call
         lore.add(ChatColor.GRAY + "Teleport " + ChatColor.GREEN + "8 blocks " + ChatColor.GRAY + "ahead of you and gain "
                 + ChatColor.GREEN + "+50" + ChatColor.WHITE + "⭐ Speed for"); // Closing the add call
         lore.add(ChatColor.GREEN + "3 seconds"); // Closing the add call
         lore.add(""); // Closing the add call
         lore.add(ChatColor.DARK_GRAY + "Cooldown " + ChatColor.GREEN + "5s"); // Closing the add call
-        lore.add(ChatColor.GRAY + "Mana cost: " + ChatColor.AQUA + "0"); // Closing the add call
+        lore.add(ChatColor.GRAY + "Mana cost: " + ChatColor.AQUA + "50"); // Closing the add call
         lore.add(""); // Closing the add call
         lore.add(ChatColor.DARK_GRAY + "This item can be reforged"); // Closing the add call
         lore.add("");
